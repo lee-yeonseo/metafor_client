0397ac97afde38c30d97a4c9f4cfa608f8efa101
jungwho <jungwho0328@gmail.com>
2024-11-22 20:34:14 +0900

    Add router example

--- client/src/utils/sign/SignInContainer.js
+++ client/src/utils/sign/SignInContainer.js
@@ -1,7 +1,17 @@
+import styled from "@emotion/styled";
+import { useRouter } from "next/router";
+
 export default function SignInContainer() {
+  const router = useRouter();
+
+  const onClickNext = () => {
+    router.push("/sign/sign-up");
+  };
   return (
     <>
-      <div>sign in</div>
+      <NextButton onClick={onClickNext}>sign in</NextButton>
     </>
   );
 }
+
+const NextButton = styled.button``;
